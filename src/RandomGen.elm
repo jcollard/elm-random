1cb03bdfbbd8b3fc93cdec449c76af94266e2c41
Evan Czaplicki <info@elm-lang.org>
2013-11-12 16:28:29 +0100

    Generalize API to allow many implementations of a generator

--- src/RandomGen.elm
+++ src/RandomGen.elm
@@ -18,16 +18,18 @@ The is almost a direct translation of Haskell's
 module which is an implemenation of the Portable Combined Generator of
 L'Ecuyer for 32-bit computers. It has a period of roughly 2.30584e18.
 
-# Creating a generator
-@docs generator
-
 # Generating Numbers
 @docs int, float, intRange, floatRange, listOf
+
+# Generators
+@docs standard, Generator
+
 -}
 
 {-| Generator provides a common interface for number generators.
 To create one, you must specify three components: next, split, range
 
+ * The `state` field holds the current state of the generator.
  * The `next` operation returns an Int that is uniformly distributed in the
    range returned by genRange (including both end points), and a new generator.
  * The `split` operation allows one to obtain two distinct random number
@@ -36,31 +38,31 @@ To create one, you must specify three components: next, split, range
    little work has been done on statistically robust implementations of split.
  * The `range` operation yields the range of values returned by the generator.
 -}
-type Generator = {
-  state : StdGen,
-  next  : StdGen -> (Int, StdGen),
-  split : StdGen -> (StdGen, StdGen),
-  range : StdGen -> (Int,Int)
+type Generator state = {
+  state : state,
+  next  : state -> (Int, state),
+  split : state -> (state, state),
+  range : state -> (Int,Int)
 }
 
-data StdGen = StdGen Int Int
+data Standard = Standard Int Int
 
-{-| Given a seed value, creates a Generator. Using the same seed will yield
-repeatable results.
+{-| Given a seed value, creates a standard Generator.
+Using the same seed will yield repeatable results.
 -}
-generator : Int -> Generator
-generator seed = Generator (mkStdGen seed) stdNext stdSplit stdRange
+standard : Int -> Generator Standard
+standard seed = Generator (mkStdGen seed) stdNext stdSplit stdRange
 
 {-| Produce the initial generator state. Distinct arguments should be likely
 to produce distinct generator states.
 -}
-mkStdGen : Int -> StdGen
+mkStdGen : Int -> Standard
 mkStdGen s' =
     let s = max s' -s'
         q  = s `div` (magicNum6-1)
         s1 = s `mod` (magicNum6-1)
         s2 = q `mod` (magicNum7-1)
-    in  StdGen (s1+1) (s2+1)                         
+    in  Standard (s1+1) (s2+1)                         
 
 {-| The maximum value for randomly generated ints -}
 maxInt : Int
@@ -80,8 +82,8 @@ magicNum6 = 2147483563
 magicNum7 = 2137383399
 magicNum8 = 2147483562
 
-stdNext : StdGen -> (Int, StdGen)
-stdNext (StdGen s1 s2) = 
+stdNext : Standard -> (Int, Standard)
+stdNext (Standard s1 s2) = 
     -- Div always rounds down and so random numbers are biased
     -- ideally we would use division that rounds towards zero so
     -- that in the negative case it rounds up and in the positive case
@@ -95,26 +97,26 @@ stdNext (StdGen s1 s2) =
         s2'' = if s2' < 0 then s2' + magicNum7 else s2'
         z = s1'' - s2''
         z' = if z < 1 then z + magicNum8 else z
-    in  (z', StdGen s1'' s2'')
+    in  (z', Standard s1'' s2'')
 
-stdSplit : StdGen -> (StdGen, StdGen)
-stdSplit (StdGen s1 s2 as std) =
+stdSplit : Standard -> (Standard, Standard)
+stdSplit (Standard s1 s2 as std) =
     let new_s1 = if s1 == magicNum6-1 then 1 else s1 + 1
         new_s2 = if s2 == 1 then magicNum7-1 else s2 - 1
-        (StdGen t1 t2) = snd (stdNext std)
-    in  (StdGen new_s1 t2, StdGen t1 new_s2)
+        (Standard t1 t2) = snd (stdNext std)
+    in  (Standard new_s1 t2, Standard t1 new_s2)
 
-stdRange : StdGen -> (Int,Int)
+stdRange : Standard -> (Int,Int)
 stdRange _ = (0, magicNum8)
 
 {-| Generate an integer in range [minInt,maxInt] inclusive. -}
-int : Generator -> (Int, Generator)
+int : Generator g -> (Int, Generator g)
 int = intRange (minInt, maxInt)
 
 {-| Generate an integer in a given range. For example, the expression
 `intRange (0,1) generator` will produce either a zero or a one.
 -}
-intRange : (Int, Int) -> Generator -> (Int, Generator)
+intRange : (Int, Int) -> Generator g -> (Int, Generator g)
 intRange (lo, hi) generator =
   if lo > hi
   then intRange (hi, lo) generator
@@ -135,11 +137,11 @@ iLogBase b i =
     if i < b then 1 else 1 + iLogBase b (i `div` b)
 
 {-| Generate a float between 0 and 1 inclusive. -}
-float : Generator -> (Float, Generator)
+float : Generator g -> (Float, Generator g)
 float = floatRange (0,1)
 
 {-| Generate a float in a given range. -}
-floatRange : (Float, Float) -> Generator -> (Float, Generator)
+floatRange : (Float, Float) -> Generator g -> (Float, Generator g)
 floatRange (lo, hi) generator = 
     if lo > hi
     then floatRange (hi, lo) generator
@@ -156,11 +158,12 @@ floatRange (lo, hi) generator =
       -- list of 42 integers in range [0,3]
       listOf (intRange (0,3)) 42 generator
 -}
-listOf : (Generator -> (number, Generator)) -> Int -> Generator -> ([number], Generator)
+listOf : (Generator g -> (number, Generator g)) ->
+         Int -> Generator g -> ([number], Generator g)
 listOf = listOfHelp []
 
-listOfHelp : [number] -> (Generator -> (number, Generator)) ->
-             Int -> Generator -> ([number], Generator)
+listOfHelp : [number] -> (Generator g -> (number, Generator g)) ->
+             Int -> Generator g -> ([number], Generator g)
 listOfHelp list generate n generator =
     if n < 1
     then (reverse list, generator)
